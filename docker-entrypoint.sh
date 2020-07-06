3f4278f2a190f0613e81062fc061cf3abfe7f1ad
Kalka <k@kalka.io>
2020-07-06 11:26:19 -0400

    test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,9 +10,7 @@ function permfix {
   if [ $UID != 1000 ]; then
     sudo groupadd -g $GID sharedsrcds
     sudo usermod -aG $GID steam
-    sudo chown -R $UID:$GID /home/steam/
-    sudo chgrp -R $GID /home/steam/
-    sudo chmod -R 770 /home/steam/
+    sudo chown -R steam:$GID /home/steam/
   else
     sudo chown -R steam:steam /home/steam/
   fi
