e19cb31f55e696b019b6e62f39a5081f1adef77a
Kalka <k@kalka.io>
2020-07-05 23:15:00 -0400

    space

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -33,7 +33,7 @@ function copy {
     do if [ -d "./$file" ]; then
       sudo mkdir -p "$TF2_TEMPDIR/$file"
     else
-      sudo cp -n "$TF2_TEMPDIR/$file" "$TF2_DIR/$file"
+      sudo cp -n "$TF2_TEMPDIR/$file " "$TF2_DIR/$file"
   fi
   done
 }
